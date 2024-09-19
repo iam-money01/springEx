a3549b3c80faf0f47657505bba9cb8c9c0af9fff
Money <selvam.money@gmail.com>
2024-09-19 19:10:54 +0530

    Updated environment bean code

--- profileEx/src/main/java/com/example/profileEx/component/ProdProfileMsg.java
+++ profileEx/src/main/java/com/example/profileEx/component/ProdProfileMsg.java
@@ -1,10 +1,12 @@
 package com.example.profileEx.component;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import jakarta.annotation.PostConstruct;
@@ -12,7 +14,9 @@ import jakarta.annotation.PostConstruct;
 @Component
 @Profile("prod")
 public class ProdProfileMsg {
-
+	
+	@Autowired
+	private Environment env;
 	
 	@Value("${message}")
 	String message;
@@ -22,7 +26,8 @@ public class ProdProfileMsg {
 	
 	@PostConstruct
 	public void display() {
-		System.out.println("Profile is set to : "+message);
-		System.out.println("Profile is set to : "+prodMsg);
+		//System.out.println("Profile is set to : "+message);
+		System.out.println(prodMsg);
+		System.out.println("Env: "+env.getProperty("test"));
 	}
 }
