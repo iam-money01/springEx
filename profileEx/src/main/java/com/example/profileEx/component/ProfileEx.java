8e384b472a2b232dfd1166fb5f695a6ae29b086b
Money <selvam.money@gmail.com>
2024-09-19 18:33:09 +0530

    updated multi profile active code

--- profileEx/src/main/java/com/example/profileEx/component/ProfileEx.java
+++ profileEx/src/main/java/com/example/profileEx/component/ProfileEx.java
@@ -3,11 +3,13 @@ package com.example.profileEx.component;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import jakarta.annotation.PostConstruct;
 
 @Component
+@PropertySource("application-prod.yml")// will not cause bean creation exception even prod spec key is not set to active
 public class ProfileEx {
 
 	
