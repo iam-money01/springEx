07df79df66551849fcfda5a0bfcc26e37e7f0511
Money <selvam.money@gmail.com>
2024-09-24 00:44:01 +0530

    Added new project for err and validation

---
+++ demoErr_Validate/src/main/java/com/example/demo/err/validate/DemoErrValidateApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.err.validate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoErrValidateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoErrValidateApplication.class, args);
+	}
+
+}
---
+++ demoErr_Validate/src/test/java/com/example/demo/err/validate/DemoErrValidateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.err.validate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoErrValidateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
